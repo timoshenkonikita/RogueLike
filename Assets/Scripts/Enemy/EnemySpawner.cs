f6a1404ea9c78ac11e908ef09f13f9a4ed4d5f7d
RomanDruzhba <98876439+RomanDruzhba@users.noreply.github.com>
2023-12-25 04:46:38 +0700

    спавн монстров

---
+++ Assets/Scripts/Enemy/EnemySpawner.cs
@@ -0,0 +1,74 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour
+{
+    [System.Serializable]
+
+    public class Wave
+    {
+        public string waveName;
+        public List<EnemyGroup> enemyGroups;
+        public int waveQuota;
+        public float spawnInterval;
+        public int spawnCount;
+    }
+
+    [System.Serializable]
+
+    public class EnemyGroup
+    {
+        public string enemyName;
+        public int enemyCount;
+        public int spawnCount;
+        public GameObject enemyPrefab;
+    }
+
+    public List<Wave> waves;
+    public int currentWaveCount;
+
+    Transform player;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //player = FindObjectOfType<PlayerStats>().transform;
+        CalculateWaveQuota();
+        SpawnEnemies();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void CalculateWaveQuota()
+    {
+        int currentWaveQuota = 0;
+        foreach (var enenmyGroup in waves[currentWaveCount].enemyGroups)
+        {
+            currentWaveQuota += enenmyGroup.enemyCount;
+        }
+        waves[currentWaveCount].waveQuota = currentWaveQuota;
+        Debug.LogWarning(currentWaveQuota);
+    }
+    void SpawnEnemies()
+    {
+        if (waves[currentWaveCount].spawnCount < waves[currentWaveCount].waveQuota)
+        {
+            foreach(var enemyGroup in waves[currentWaveCount].enemyGroups)
+            {
+                if(enemyGroup.spawnCount < enemyGroup.enemyCount)
+                {
+                    Vector2 spawnPosition = new Vector2(player.transform.position.x + Random.Range(-10f, 10f), player.transform.position.y + Random.Range(-10f, 10f));
+                    Instantiate(enemyGroup.enemyPrefab, spawnPosition, Quaternion.identity);
+
+                    enemyGroup.spawnCount++;
+                    waves[currentWaveCount].spawnCount++;
+                }
+            }
+        }
+    }
+}
